1a15831d33e7dbd6171e10ee4574d1ba8c2997a5
Yuri Bueno <yuribueno10@hotmail.com>
2022-05-12 20:32:36 -0300

    Adicionado função desfazer e persistência de dados

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-home',
@@ -6,19 +8,49 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  tarefas: string[] = 
+  tarefas: string[] = [];
+  tarefasBackup: string[] = [];
 
-  ];
 
   novaTarefa: string = '';
-  constructor() { }
-
-  cadastrarTarefa() {
-    this.tarefas.push(this.novaTarefa);
-    this.novaTarefa = '';
+  constructor(private storage: Storage, private toast: ToastController) {
+    this.iniciarBanco();
 
   }
-  removerTarefa(posicao) {
+
+  async desfazer(tarefaExcluida) {
+    const t = await this.toast.create({
+      message: "Excluído: " + tarefaExcluida,
+      duration: 3000,
+      buttons: [
+        {
+          text: "Desfazer",
+          handler: async () => {
+            this.tarefas = [...this.tarefasBackup];
+            await this.storage.set('tarefas', this.tarefas);
+          },
+
+        }]
+    });
+    t.present();
+
+  }
+
+  async iniciarBanco() {
+    await this.storage.create();
+    this.tarefas = await this.storage.get('tarefas') ?? [];
+  }
+
+  async cadastrarTarefa() {
+    this.tarefas.push(this.novaTarefa);
+    this.novaTarefa = '';
+    await this.storage.set('tarefas', this.tarefas);
+
+  }
+  async removerTarefa(posicao) {
+    this.desfazer(this.tarefas[posicao]);
+    this.tarefasBackup = [...this.tarefas];
     this.tarefas.splice(posicao, 1);
+    await this.storage.set('tarefas', this.tarefas);
   }
 }
